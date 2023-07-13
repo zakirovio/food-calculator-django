48c172249455597a3750b49233ba40ba36e09876
iamkhaidarzakirov <root@edu.io>
2023-07-13 11:08:50 +0300

    Update import to sqlite3 func...s

--- import.py
+++ import.py
@@ -2,11 +2,18 @@ import sqlite3
 import pandas as pd
 
 
-def import_tosqlite3(db_path: str, table_path: str, db_table_name: str, sheet_name: bool = None, index: bool = False, method: str = 'append'):
+def import_tosqlite3(
+        db_path: str,
+        table_path: str,
+        db_table_name: str,
+        sheet_name: bool = None,
+        index: bool = False
+):
+
     with sqlite3.connect(db_path) as connect:
         wb = pd.read_excel(table_path, sheet_name=sheet_name)
         for sheet in wb:
-            wb[sheet].to_sql(name=db_table_name, if_exists=method, con=connect, index=index)
+            wb[sheet].to_sql(name=db_table_name, if_exists='append', con=connect, index=index)
 
 
 def import_tomysql():
@@ -14,5 +21,4 @@ def import_tomysql():
 
 
 if __name__ == '__main__':
-    ...
-    
+    print('[INFO: ...start import.]')
